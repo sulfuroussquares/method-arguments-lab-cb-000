4039709d53aa1b58fc20f1db4f00b94d146dee1f
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-27 17:10:33 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(name)
+ puts "Hi, my name is #{name}.\n"
+end
+
+
+def introduction_with_language(name, phrase)
+  puts "Hi, my name is #{name} and I am learning to program in #{phrase}.\n"
+end
