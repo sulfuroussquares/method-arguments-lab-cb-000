1abdd89c5d66c757b2b22ca7ac51953b29be4275
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-27 17:09:47 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
 def introduction_with_language(name, phrase)
-  puts "Hi, my name is {name} and I am learning to program in {phrase}.\n"
+  puts "Hi, my name is #{name} and I am learning to program in #{phrase}.\n"
 end
